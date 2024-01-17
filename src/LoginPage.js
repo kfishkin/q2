a98ccfd95ebe4f75a4a13cbfbbeaf35396dc0b9e
Ken Fishkin <ken.fishkin@gmail.com>
2024-01-17 10:34:48 -0800

    checkpoint

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 
 
 // props:
@@ -18,13 +18,100 @@ class LoginPage extends React.Component {
   }
 
   createUser() {
-    this.setState({makingNewAccount: true});
+    this.setState({ makingNewAccount: true });
 
   }
 
   showNewAccountPage() {
+    const onFinish = (values) => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
     return (
-      <div>make a new account</div>
+      <div>
+        <h1>Hello, new player...</h1>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="handle"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Display name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your display name',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email contact info',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>                    
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div >
     )
   }
 
@@ -99,8 +186,8 @@ class LoginPage extends React.Component {
         </Form>
         <hr />
         <div>
-        Or <button onClick={(e) => this.createUser()}>Create a new user</button>
-      </div>
+          Or <button onClick={(e) => this.createUser()}>Create a new user</button>
+        </div>
       </div >
     );
   }
