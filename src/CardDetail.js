92646b316c5acefce044e135ceffb4aa357dbf8d
Ken Fishkin <ken.fishkin@gmail.com>
2024-02-05 11:28:30 -0800

    detail card is less ugly

--- src/CardDetail.js
+++ src/CardDetail.js
@@ -13,6 +13,31 @@ class CardDetail extends React.Component {
     let card = this.props.card;
     let gc = card.game_card;
     let typeObj = CardType.make(gc.type)
+    return (
+      <div className='card_face_border'>
+        <div className='card_face_title'>
+          {gc.display_name}
+        </div>
+        <div className='card_face_level_image'>
+        <img src="pix/general/yellow_star.jpg" width="32"/>  
+        </div>
+        <div className='card_face_level_value'>
+          {gc.level}
+        </div>
+        <div className='card_face_value'>
+          (${gc.sell_value})
+        </div>
+        <div className='card_face_battle_image'>
+        <img src="pix/general/sword_icon.png" width="32"/>  
+        </div>
+        <div className='card_face_battle_value'>
+          {gc.battle_value}
+        </div>
+        <div className='card_face_description'>
+          {typeObj.FullyDescribe(card, this.props.gameInfo, this.props.deck)}
+        </div>
+      </div>
+    )
     let line1 = <span>This is a level {gc.level} <i>{typeObj.AltText()}</i> card.</span>;
     let line2 = <span>It has a battle value of <b>{gc.battle_value}</b></span>;
     let line3 = typeObj.FullyDescribe(card, this.props.gameInfo, this.props.deck);
